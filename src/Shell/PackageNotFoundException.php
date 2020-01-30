77e7e1f74f51a3d26530c0e3f4a9d847019adba6
Michael Casey <mcasey@magento.com>
2020-01-30 10:45:13 -0600

    Fix grammar in DocBlock (minor)

--- src/Shell/PackageNotFoundException.php
+++ src/Shell/PackageNotFoundException.php
@@ -10,7 +10,7 @@ namespace Magento\CloudPatches\Shell;
 use Magento\CloudPatches\App\GenericException;
 
 /**
- * Exception if a composer package could not be found for some reason (eg, symfony/process)
+ * Exception if a Composer package could not be found for some reason (e.g., symfony/process).
  */
 class PackageNotFoundException extends GenericException
 {
