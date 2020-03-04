0511824bdb10b7a7138b5039f2c2159fd4e7c963
Bohdan Korablov <korablov@adobe.com>
2020-03-04 12:27:42 -0600

    MAGECLOUD-5478: Add acceptance function tests for magento-cloud-patches

---
+++ tests/travis/functional.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright © Magento, Inc. All rights reserved.
+# See COPYING.txt for license details.
+
+set -e
+trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
+
+case $TRAVIS_PHP_VERSION in
+    7.1)
+        #./vendor/bin/codecept run -g php71 --steps
+        echo "$TRAVIS_PHP_VERSION"
+        ;;
+    7.2)
+        #./vendor/bin/codecept run -g php72 --steps
+        echo "$TRAVIS_PHP_VERSION"
+        ;;
+    7.3)
+        #./vendor/bin/codecept run -g php73 --steps
+        echo "$TRAVIS_PHP_VERSION"
+        ;;
+esac
