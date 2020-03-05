c876990b29e21429bac2dcee68a938dccc7b917e
Oleg Posyniak <posyniak@adobe.com>
2020-03-05 16:04:07 -0600

    MAGECLOUD-5478: Add acceptance function tests for magento-cloud-patches (#32)

---
+++ src/Test/Functional/Acceptance/Acceptance71Cest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CloudPatches\Test\Functional\Acceptance;
+
+/**
+ * @group php71
+ */
+class Acceptance71Cest extends AcceptanceCest
+{
+    /**
+     * @return array
+     */
+    protected function patchesDataProvider(): array
+    {
+        return [
+            ['magentoVersion' => '2.1.16'],
+            ['magentoVersion' => '2.1.18'],
+            ['magentoVersion' => '2.2.0'],
+            ['magentoVersion' => '2.2.11'],
+        ];
+    }
+}
---
+++ src/Test/Functional/Acceptance/AcceptanceCest.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CloudPatches\Test\Functional\Acceptance;
+
+/**
+ * @group php73
+ */
+class AcceptanceCest
+{
+    /**
+     * @param \CliTester $I
+     */
+    public function _before(\CliTester $I): void
+    {
+        $I->cleanupWorkDir();
+    }
+
+    /**
+     * @param \CliTester $I
+     * @param string $magentoVersion
+     */
+    protected function prepareTemplate(\CliTester $I, string $magentoVersion): void
+    {
+        $I->cloneTemplateToWorkDir($magentoVersion);
+        $I->createAuthJson();
+        $I->createArtifactsDir();
+        $I->createArtifactCurrentTestedCode('patches', '1.0.99');
+        $I->addArtifactsRepoToComposer();
+        $I->addEceDockerGitRepoToComposer();
+        $I->addDependencyToComposer('magento/magento-cloud-patches', '1.0.99');
+        $I->addDependencyToComposer(
+            'magento/magento-cloud-docker',
+            $I->getDependencyVersion('magento/magento-cloud-docker')
+        );
+        $I->composerUpdate();
+    }
+
+    /**
+     * @param \CliTester $I
+     * @param \Codeception\Example $data
+     * @throws \Robo\Exception\TaskException
+     * @dataProvider patchesDataProvider
+     */
+    public function testPatches(\CliTester $I, \Codeception\Example $data): void
+    {
+        $this->prepareTemplate($I, $data['magentoVersion']);
+        $this->removeESIfExists($I);
+        $I->runEceDockerCommand('build:compose --mode=production');
+        $I->runDockerComposeCommand('run build cloud-build');
+        $I->startEnvironment();
+        $I->runDockerComposeCommand('run deploy cloud-deploy');
+        $I->runDockerComposeCommand('run deploy cloud-post-deploy');
+        $I->amOnPage('/');
+        $I->see('Home page');
+        $I->see('CMS homepage content goes here.');
+    }
+
+    /**
+     * @param \CliTester $I
+     */
+    protected function removeESIfExists(\CliTester $I): void
+    {
+        $services = $I->readServicesYaml();
+
+        if (isset($services['elasticsearch'])) {
+            unset($services['elasticsearch']);
+            $I->writeServicesYaml($services);
+
+            $app = $I->readAppMagentoYaml();
+            unset($app['relationships']['elasticsearch']);
+            $I->writeAppMagentoYaml($app);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function patchesDataProvider(): array
+    {
+        return [
+            ['magentoVersion' => '2.3.3'],
+            ['magentoVersion' => 'master'],
+        ];
+    }
+
+    /**
+     * @param \CliTester $I
+     */
+    public function _after(\CliTester $I): void
+    {
+        $I->stopEnvironment();
+        $I->removeWorkDir();
+    }
+}
