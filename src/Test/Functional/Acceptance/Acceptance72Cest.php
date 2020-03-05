2a467a3b2a80c06babbe5c20b82f030289fd3b76
Oleg Posyniak <posyniak@adobe.com>
2020-03-05 16:04:07 -0600

    MAGECLOUD-5478: Add acceptance function tests for magento-cloud-patches (#32)

---
+++ src/Test/Functional/Acceptance/Acceptance72Cest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CloudPatches\Test\Functional\Acceptance;
+
+/**
+ * @group php72
+ */
+class Acceptance72Cest extends AcceptanceCest
+{
+    /**
+     * @return array
+     */
+    protected function patchesDataProvider(): array
+    {
+        return [
+            ['magentoVersion' => '2.3.0'],
+            ['magentoVersion' => '2.3.2'],
+        ];
+    }
+}
