fb3372ced50cc90f48ae57af0947debc38db2c44
Bohdan Korablov <korablov@adobe.com>
2020-05-21 16:12:30 -0500

    MCLOUD-5953: Backport Redis patches

--- src/Test/Functional/Acceptance/AcceptanceCest.php
+++ src/Test/Functional/Acceptance/AcceptanceCest.php
@@ -10,49 +10,8 @@ namespace Magento\CloudPatches\Test\Functional\Acceptance;
 /**
  * @group php73
  */
-class AcceptanceCest
+class AcceptanceCest extends AbstractCest
 {
-    /**
-     * @var string
-     */
-    protected $edition = 'EE';
-
-    /**
-     * @param \CliTester $I
-     */
-    public function _before(\CliTester $I): void
-    {
-        $I->cleanupWorkDir();
-    }
-
-    /**
-     * @param \CliTester $I
-     * @param string $templateVersion
-     */
-    protected function prepareTemplate(\CliTester $I, string $templateVersion): void
-    {
-        $I->cloneTemplateToWorkDir($templateVersion);
-        $I->createAuthJson();
-        $I->createArtifactsDir();
-        $I->createArtifactCurrentTestedCode('patches', '1.0.99');
-        $I->addArtifactsRepoToComposer();
-        $I->addEceDockerGitRepoToComposer();
-        $I->addDependencyToComposer('magento/magento-cloud-patches', '1.0.99');
-        $I->addDependencyToComposer(
-            'magento/magento-cloud-docker',
-            $I->getDependencyVersion('magento/magento-cloud-docker')
-        );
-
-        if ($this->edition === 'CE') {
-            $version = $this->getVersionRangeForMagento($I);
-            $I->removeDependencyFromComposer('magento/magento-cloud-metapackage');
-            $I->addDependencyToComposer('magento/ece-tools', '^2002.1.0');
-            $I->addDependencyToComposer('magento/product-community-edition', $version);
-        }
-
-        $I->composerUpdate();
-    }
-
     /**
      * @param \CliTester $I
      * @return string
@@ -94,14 +53,5 @@ class AcceptanceCest
             ['templateVersion' => '2.3.5'],
             ['templateVersion' => 'master'],
         ];
-    }
-
-    /**
-     * @param \CliTester $I
-     */
-    public function _after(\CliTester $I): void
-    {
-        $I->stopEnvironment();
-        $I->removeWorkDir();
     }
 }
