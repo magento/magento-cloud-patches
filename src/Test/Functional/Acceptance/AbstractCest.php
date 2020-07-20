cea5d99dd991973a1e27bb896d60468c93f43db9
Bohdan Korablov <korablov@adobe.com>
2020-07-20 09:28:29 -0500

    MCLOUD-6455: Increase test coverage for magento-cloud-patches and quality-patches (#58)

--- src/Test/Functional/Acceptance/AbstractCest.php
+++ src/Test/Functional/Acceptance/AbstractCest.php
@@ -28,8 +28,9 @@ class AbstractCest
     /**
      * @param \CliTester $I
      * @param string $templateVersion
+     * @param string $magentoVersion
      */
-    protected function prepareTemplate(\CliTester $I, string $templateVersion): void
+    protected function prepareTemplate(\CliTester $I, string $templateVersion, string $magentoVersion = null): void
     {
         $I->cloneTemplateToWorkDir($templateVersion);
         $I->createAuthJson();
@@ -38,6 +39,7 @@ class AbstractCest
         $I->addArtifactsRepoToComposer();
         $I->addEceDockerGitRepoToComposer();
         $I->addQualityPatchesGitRepoToComposer();
+        $I->addEceToolsGitRepoToComposer();
         $I->addDependencyToComposer('magento/magento-cloud-patches', '1.0.99');
         $I->addDependencyToComposer(
             'magento/magento-cloud-docker',
@@ -48,11 +50,15 @@ class AbstractCest
             $I->getDependencyVersion('magento/quality-patches')
         );
 
-        if ($this->edition === 'CE') {
-            $version = $this->getVersionRangeForMagento($I);
+        $I->addDependencyToComposer('magento/ece-tools', 'dev-develop as 2002.1.99');
+
+        if ($this->edition === 'CE' || $magentoVersion) {
+            $version = $magentoVersion ?: $this->getVersionRangeForMagento($I);
             $I->removeDependencyFromComposer('magento/magento-cloud-metapackage');
-            $I->addDependencyToComposer('magento/ece-tools', '^2002.1.0');
-            $I->addDependencyToComposer('magento/product-community-edition', $version);
+            $I->addDependencyToComposer(
+                $this->edition === 'CE' ? 'magento/product-community-edition' : 'magento/product-enterprise-edition',
+                $version
+            );
         }
 
         $I->composerUpdate();
@@ -65,6 +71,17 @@ class AbstractCest
     protected function convertEnvFromArrayToJson(array $data): string
     {
         return addslashes(json_encode($data));
+    }
+
+    /**
+     * @param \CliTester $I
+     * @return string
+     */
+    protected function getVersionRangeForMagento(\CliTester $I): string
+    {
+        $composer = json_decode(file_get_contents($I->getWorkDirPath() . '/composer.json'), true);
+
+        return $composer['require']['magento/magento-cloud-metapackage'] ?? '';
     }
 
     /**
