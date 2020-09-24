60a7dabf79490694af2bdd671422a13cd4889ee9
Buba Suma <soumah@adobe.com>
2020-09-24 18:17:24 -0500

    MC-37324: Add fallback to 'patch' command when 'git' command is not available
    - Remove dead code

--- src/Shell/Command/DriverException.php
+++ src/Shell/Command/DriverException.php
@@ -8,48 +8,10 @@ declare(strict_types=1);
 namespace Magento\CloudPatches\Shell\Command;
 
 use Magento\CloudPatches\Patch\PatchCommandException;
-use Throwable;
 
 /**
  * Patch command driver exception
  */
 class DriverException extends PatchCommandException
 {
-    /**
-     * @param string $message
-     * @param int $code
-     * @param Throwable|null $previous
-     */
-    public function __construct(string $message, int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($this->formatMessage($message), $code, $previous);
-    }
-
-    /**
-     * Format error message
-     *
-     * @param string $message
-     * @return string
-     */
-    private function formatMessage(string $message): string
-    {
-        $result = $message;
-        $errorMsg = null;
-        $generalMsg = null;
-        if (preg_match('#^.*?Error Output:(?<errors>.*?)$#is', $result, $matches)) {
-            $errorMsg = 'Error Output:' . $matches['errors'];
-            $result = str_replace($errorMsg, '', $result);
-            if (!trim(str_replace('=', '', $matches['errors']))) {
-                $errorMsg = null;
-            }
-        }
-        if (empty($errorMsg) && preg_match('#^.*?Output:(?<errors>.*?)$#is', $result, $matches)) {
-            $generalMsg = 'Output:' . $matches['errors'];
-            if (!trim(str_replace('=', '', $matches['errors']))) {
-                $generalMsg = null;
-            }
-        }
-
-        return $errorMsg ?? $generalMsg ?? $message;
-    }
 }
