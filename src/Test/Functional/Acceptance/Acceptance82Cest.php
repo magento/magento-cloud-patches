ca1c0948e9587002613181a9b564b3006bb25c13
Shambhu Kumar <glo72880@adobe.com>
2024-03-18 21:39:07 +0530

    Updated testcase

--- src/Test/Functional/Acceptance/Acceptance82Cest.php
+++ src/Test/Functional/Acceptance/Acceptance82Cest.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 namespace Magento\CloudPatches\Test\Functional\Acceptance;
 
 /**
- * @group php83
+ * @group php82
  */
 class AcceptanceCest extends AbstractCest
 {
@@ -49,7 +49,9 @@ class AcceptanceCest extends AbstractCest
     protected function patchesDataProvider(): array
     {
         return [
-            ['templateVersion' => '2.4.7-beta-test', 'magentoVersion' => '2.4.7-beta401'],
+            ['templateVersion' => '2.4.6', 'magentoVersion' => '2.4.6'],
+            ['templateVersion' => '2.4.6', 'magentoVersion' => '2.4.6-p1'],
+            ['templateVersion' => '2.4.7-beta', 'magentoVersion' => null],
         ];
     }
 }
