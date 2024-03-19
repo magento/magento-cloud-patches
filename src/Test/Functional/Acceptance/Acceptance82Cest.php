29a1c107e8a477928fc247a2caf5e0f8f3adbca0
Sivaram Manijeganathan <sivaramm@adobe.com>
2024-03-19 10:05:41 -0500

    php 82 test file fix

--- src/Test/Functional/Acceptance/Acceptance82Cest.php
+++ src/Test/Functional/Acceptance/Acceptance82Cest.php
@@ -10,39 +10,8 @@ namespace Magento\CloudPatches\Test\Functional\Acceptance;
 /**
  * @group php82
  */
-class AcceptanceCest extends AbstractCest
+class Acceptance82Cest extends AcceptanceCest
 {
-    /**
-     * @param \CliTester $I
-     */
-    public function _before(\CliTester $I): void
-    {
-        parent::_before($I);
-    }
-
-    /**
-     * @param \CliTester $I
-     * @param \Codeception\Example $data
-     * @throws \Robo\Exception\TaskException
-     * @dataProvider patchesDataProvider
-     */
-    public function testPatches(\CliTester $I, \Codeception\Example $data): void
-    {
-        $this->prepareTemplate($I, $data['templateVersion'], $data['magentoVersion'] ?? null);
-        $I->copyFileToWorkDir('files/patches/.apply_quality_patches.env.yaml', '.magento.env.yaml');
-        $I->generateDockerCompose(sprintf(
-            '--mode=production --env-vars="%s"',
-            $this->convertEnvFromArrayToJson(['MAGENTO_CLOUD_PROJECT' => 'travis-testing'])
-        ));
-        $I->assertTrue($I->runDockerComposeCommand('run build cloud-build'));
-        $I->assertTrue($I->startEnvironment());
-        $I->assertTrue($I->runDockerComposeCommand('run deploy cloud-deploy'));
-        $I->assertTrue($I->runDockerComposeCommand('run deploy cloud-post-deploy'));
-        $I->amOnPage('/');
-        $I->see('Home page');
-        $I->see('CMS homepage content goes here.');
-    }
-
     /**
      * @return array
      */
