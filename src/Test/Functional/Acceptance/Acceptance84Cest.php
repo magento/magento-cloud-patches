decb1fb07b9ef6567d907f08fcb316c8749193ad
glo42671 <glo42671@adobe.com>
2025-01-27 21:24:09 +0530

    Add support of php 8.4 to  cloud-patches

---
+++ src/Test/Functional/Acceptance/Acceptance84Cest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CloudPatches\Test\Functional\Acceptance;
+
+/**
+ * @group php84
+ */
+class Acceptance84Cest extends AcceptanceCest
+{
+    /**
+     * @return array
+     */
+    protected function patchesDataProvider(): array
+    {
+        return [
+            ['templateVersion' => '2.4.8', 'magentoVersion' => '2.4.8'],
+        ];
+    }
+}
