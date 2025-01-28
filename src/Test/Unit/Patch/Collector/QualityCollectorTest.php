24a0d768e34ecee279054e7ef2e8a9912e95ee52
glo42671 <glo42671@adobe.com>
2025-01-28 21:13:20 +0530

    Add support of php 8.4 to  cloud-patches

--- src/Test/Unit/Patch/Collector/QualityCollectorTest.php
+++ src/Test/Unit/Patch/Collector/QualityCollectorTest.php
@@ -223,7 +223,7 @@ class QualityCollectorTest extends TestCase
         $config = require __DIR__ . '/Fixture/quality_config_invalid.php';
 
         $expectedExceptionMessage = 'Patch MDVA-2033 has invalid configuration:' .
-            PHP_EOL . ' - Property \'file\' is not found in \'2.2.0 - 2.2.5\' ' .
+            PHP_EOL . ' - Property \'file\' is not found in \'2.2.0 - 2.2.5\'' .
             PHP_EOL . ' - Property \'require\' from \'2.2.0 - 2.2.5\' should have an array type' .
             PHP_EOL . ' - Property \'replaced-with\' from \'2.2.0 - 2.2.5\' should have a string type' .
             PHP_EOL . ' - Property \'deprecated\' from \'2.2.0 - 2.2.5\' should have a boolean type';
