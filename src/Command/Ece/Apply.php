111c8c95db04de1c94a9d75e61a2be8556f56f87
Prateek Karanpuria <pru88413@adobe.com>
2025-05-29 17:34:38 +0530

    MCLOUD-13707: Unit test fixes for all PHP versions

--- src/Command/Ece/Apply.php
+++ src/Command/Ece/Apply.php
@@ -89,7 +89,7 @@ class Apply extends AbstractCommand
     /**
      * @inheritDoc
      */
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->logger->info($this->magentoVersion->get());
 
