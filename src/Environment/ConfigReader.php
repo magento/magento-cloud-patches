7df8fe4b58a1f5f0f571923760bb79f130d4316f
Prateek Karanpuria <pru88413@adobe.com>
2025-10-23 17:38:55 +0530

    MCLOUD-14020: Cosmetic updates

--- src/Environment/ConfigReader.php
+++ src/Environment/ConfigReader.php
@@ -100,7 +100,7 @@ class ConfigReader
     private function normalizeYamlData(mixed $data): mixed
     {
         if ($data instanceof TaggedValue) {
-            $tag = $data->getTag();   // e.g. "php/const:\PDO::MYSQL_ATTR_LOCAL_INFILE"
+            $tag   = $data->getTag();   // e.g. "php/const:\PDO::MYSQL_ATTR_LOCAL_INFILE"
             $value = $data->getValue();
 
             // Handle php/const tags (Symfony strips leading '!')
